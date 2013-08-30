67d792e225661c6bb727558639a1de9098138175
Stephan Leroux <stephanleroux@gmail.com>
2013-08-30 17:48:47 -0400

    Created project

---
+++ SLDropdownViewController/SLDropdownViewController/SLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SLAppDelegate.h
+//  SLDropdownViewController
+//
+//  Created by Stephan Leroux on 2013-08-30.
+//  Copyright (c) 2013 Stephan Leroux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SLDropdownViewController/SLDropdownViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SLDropdownViewController
+//
+//  Created by Stephan Leroux on 2013-08-30.
+//  Copyright (c) 2013 Stephan Leroux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SLAppDelegate class]));
+    }
+}
